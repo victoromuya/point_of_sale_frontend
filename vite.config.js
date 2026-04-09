ec08aea6fcebe3a17ab8f8026c2f9dea416e4265
unknown <viczik16@gmail.com>
2026-04-09 17:52:08 +0100

    edited vite.congig

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,34 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import path from 'path'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-  build: {
-    outDir: path.resolve(__dirname, '../ggi/staticfiles_build/static'),
-    emptyOutDir: true,
-    assetsDir: 'assets',
-  },
-  server: {
-    port: 5173,
-    strictPort: true,
-    proxy: {
-      '/api': 'http://localhost:8000',
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // The third parameter '' allows loading variables WITHOUT the 'VITE_' prefix.
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [
+      react(),
+      babel({ presets: [reactCompilerPreset()] })
+    ],
+    build: {
+      outDir: path.resolve(__dirname, '../ggi/staticfiles_build/static'),
+      emptyOutDir: true,
+      assetsDir: 'assets',
     },
-  },
+    server: {
+      port: 5173,
+      strictPort: true,
+      proxy: {
+        // Use the loaded API variable here
+        '/api': {
+          target: env.API || 'http://localhost:8000',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+  }
 })
